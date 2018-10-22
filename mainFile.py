ea6cbd59495b7bf3314175a4c2fca313bac7f4b5
Udaikaran Singh <www.udaikaran@gmail.com>
2018-10-22 10:41:14 -0700

    added main file

---
+++ mainFile.py
@@ -0,0 +1,40 @@
+import neuralnet
+import numpy as np
+import pickle
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+import gradientchecker
+
+"""
+This is the main file to run all the experiments
+
+Class: CSE 190
+Assignment: PA2
+"""
+
+print("Part B: Gradient Checker")
+print()
+gradientchecker.main()
+print()
+
+print("Part C: Finding Optimal # of Epochs")
+print()
+#run problem C
+print()
+
+print("Part D: Experiment with Regularization")
+print()
+#run problem D
+print()
+
+print("Part E: Experiment with Activations")
+print()
+#run problem E
+print()
+
+print("Part F: Experiment with Network Topology")
+print()
+#run problem F
+print()
+
